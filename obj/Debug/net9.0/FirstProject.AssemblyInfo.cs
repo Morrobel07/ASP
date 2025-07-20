2c62a773796177fd2420681b4ee3f7975d92f6c5
Morrobel07 <morrobelc03@gmail.com>
2025-07-20 16:18:52 -0400

    last

--- obj/Debug/net9.0/FirstProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/FirstProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dea42a025d6d20d18b80b58dd44db76ac945e5ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a99c34e97169db28a71b33c7f71540277f238d9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
